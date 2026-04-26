74388d587cce534d2b8dd3a7b7c9be27eaed4338
Chloe Aujoulat <chloe.aujoulat19@gmail.com>
2026-04-26 23:13:59 +0200

    add python script with a few modifications about ? in url and comments about data

--- notebooks/main.py
+++ notebooks/main.py
@@ -32,17 +32,35 @@ headers = {
     'api-key': api_key
 }
 
-#url = 'https://api.ncbi.nlm.nih.gov/datasets/v2/gene/accession/NM_021803.4?api-key=api_key'
-url = 'https://api.ncbi.nlm.nih.gov/datasets/v2/gene/id/2?api-key=api_key/orthologs'
+url = 'https://api.ncbi.nlm.nih.gov/datasets/v2/gene/accession/NM_021803.4'
+#url = 'https://api.ncbi.nlm.nih.gov/datasets/v2/gene/id/2/orthologs'
+#url = 'https://api.ncbi.nlm.nih.gov/datasets/v2/gene/id/2'
+
+# '?': after, there are parameters.
 
 r = requests.get(url, headers = headers)
 print(r.json())
 
+# Download a genome and then store it in a file
+# When I need data from the API:
+# Shall I store the data, download them and store them, or shall I send them directly to treat them?
+# => It depends on the size of the data, and the version of the data we need.
+# Ex: If I have a genome which is 20Go size, it may be long to download and long to transfer from the server to the computer.
+# In this case, we usually download the data and store it (in a file, on the computer).
+# CPU manages everything; the data are either in RAM, either in disk, either in the network.
+# To transfer data from disk to network, we have to use the CPU and RAM.
 
-#dictionary = r.json()
-#for k in dictionary.keys():
-    #print(k) 
+# API requests: the data is in the network: so, it will be sent to the RAM (mandatory).
+# There is no direct flux from the network to the disk.
+# When we do a HTTP request, the data are in the RAM, not in the disk.
+# If we want to put the data into the disk, we have to create a file with write method, and write the content of the RAM
+# into the disk.
+# Ex: 20Go: retrieve small part by small part, and store these small packages into the disk. Then, we delete the packages 
+# which are in the RAM. Python deletes the packages from the RAM on its own.
 
+# When shall we store data on the disk?
+# Ex: If we want to get the temperature of the day: storing it on the disk for 10 years is not useful.
+# Information updated regularly => No need to store it on the disk.
 
 #def info_gene(siren): # The definition of a function often includes a list of parameters. 
     # These a always VARIABLES, which will receive their value when the function will be called.
